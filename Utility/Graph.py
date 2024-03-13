0df967b6943487be6891f7edb5ef4420fcda0b5c
SimoneAvellino <s.avellino@gmail.com>
2024-03-13 15:33:47 +0100

    Label memoization

--- Utility/Graph.py
+++ Utility/Graph.py
@@ -15,35 +15,22 @@ class MultiDiGraph(nx.MultiDiGraph):
         super().__init__(incoming_graph_data, multigraph_input, **attr)
         self.node_labels = None
         self.edge_labels = None
-        
-    
-    def get_all_node_labels(self):
-        return  sorted(set(flat_map([self.nodes[node]['labels'] for node in self.nodes])))
-
-    def get_all_edge_labels(self):
-        return  sorted(set([self.get_edge_data(edge[0], edge[1], edge[2])['label'] for edge in self.edges]))
     
     def get_edge_labels(self, source, destination):
         labels = []
         if(self.has_edge(source, destination)):
-            labels.extend([edge_data.get('label') for edge_data in self[source][destination].values()])
+            labels.extend([edge_data.get('type') for edge_data in self[source][destination].values()])
         return sorted(set(labels))
     
     def get_node_labels(self, id):
         return sorted(set(self.nodes[id]["labels"]))
     
-    
+    def get_all_node_labels(self):
+        if self.node_labels == None:
+            self.node_labels = sorted(set(flat_map([self.nodes[node]['labels'] for node in self.nodes])))
+        return self.node_labels
 
-    
-    # def get_all_node_labels(self):
-    #     if self.node_labels == None:
-    #         self.node_labels = sorted(set(flat_map([self.nodes[node]['labels'] for node in self.nodes])))
-    #     return self.node_labels
-
-    # def get_all_edge_labels(self):
-    #     if self.edge_labels is None:
-    #         self.edge_labels = sorted(set([self.get_edge_data(edge[0], edge[1], edge[2])['label'] for edge in self.edges]))
-    #     return self.edge_labels
-    
-    
-    
+    def get_all_edge_labels(self):
+        if self.edge_labels is None:
+            self.edge_labels = sorted(set([self.get_edge_data(edge[0], edge[1], edge[2])['type'] for edge in self.edges]))
+        return self.edge_labels
