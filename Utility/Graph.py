9608eae63fcc94381015b0439eb1a5b4c2d94ad4
SimoneAvellino <s.avellino@gmail.com>
2024-03-26 15:25:29 +0100

    Graph fix

--- Utility/Graph.py
+++ Utility/Graph.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import random
 
+# TO-DO: try to remove the check len(edge) == 3 in are_equivalent
+
 """ Utility function
 """
 
@@ -50,64 +52,44 @@ class MultiDiGraph(nx.MultiDiGraph):
 
         # Add nodes with random labels
         for i in range(num_nodes):
-            label = random.choice(self.get_all_node_labels())
-            G.add_node(node_for_adding=i, labels=[label])
+            labels = random.choice(self.get_all_node_labels())
+            self.add_node(node_for_adding=i, labels=[labels])
 
         # Add edges with random labels
         for _ in range(num_edges):
             u, v = random.sample(range(num_nodes), 2)
-            label = random.choice(self.get_all_edge_labels())
-            G.add_edge(u, v, type=label)
+            labels = random.choice(self.get_all_edge_labels())
+            self.add_edge(u, v, type=labels)
 
         return G
 
-    def are_equivalent(node1, node2, G):
+    def are_equivalent(self, node1, node2):
         # Verifica se le etichette di node1 sono un sottoinsieme o uguali alle etichette di node2
-        if set(G.nodes[node1]['label']) == set(G.nodes[node2]['label']):
+        if set(self.nodes[node1]['labels']) == set(self.nodes[node2]['labels']):
             # Verifica se gli attributi degli archi in uscita sono gli stessi
 
-            out_edges_node1 = sorted([G.edges[edge]['type'] for edge in G.out_edges(node1)])
-            out_edges_node2 = sorted([G.edges[edge]['type'] for edge in G.out_edges(node2)])
+            out_edges_node1 = sorted([self.edges[edge]['type'] for edge in self.out_edges(node1) if len(edge) == 3])
+            out_edges_node2 = sorted([self.edges[edge]['type'] for edge in self.out_edges(node2) if len(edge) == 3])
 
             if out_edges_node1 != out_edges_node2:
                 return False
 
             # Verifica se gli attributi degli archi in entrata sono gli stessi
-            in_edges_node1 = sorted([G.edges[edge]['type'] for edge in G.in_edges(node1)])
-            in_edges_node2 = sorted([G.edges[edge]['type'] for edge in G.in_edges(node2)])
+            in_edges_node1 = sorted([self.edges[edge]['type'] for edge in self.in_edges(node1) if len(edge) == 3])
+            in_edges_node2 = sorted([self.edges[edge]['type'] for edge in self.in_edges(node2) if len(edge) == 3])
 
             if in_edges_node1 != in_edges_node2:
                 return False
             return True
         else:
-
             return False
 
-    def get_labels(self, graph):
-        # Utilizza le funzionalità di networkx per ottenere le etichette dei nodi
-        node_labels = nx.get_node_attributes(graph, 'label')
-
-        # Dizionari per memorizzare le etichette degli archi uscenti e entranti
-        edge_out_labels = {}
-        edge_in_labels = {}
-
-        # Itera attraverso tutti gli archi del grafo
-        for source_node, target_node, edge_data in graph.edges(data=True):
-            # Aggiunge l'etichetta dell'arco (tipo) al dizionario degli archi uscenti
-            edge_out_labels.setdefault(source_node, {})[target_node] = edge_data.get('type', '')
-            # Aggiunge l'etichetta dell'arco (tipo) al dizionario degli archi entranti
-            edge_in_labels.setdefault(target_node, {})[source_node] = edge_data.get('type', '')
-
-        # Restituisce i dizionari contenenti le etichette dei nodi, degli archi uscenti e degli archi entranti
-
-        return node_labels, edge_out_labels, edge_in_labels
-
-    def compute_orbits(self, graph):
+    def compute_orbits(self):
         # Lista per memorizzare le orbite
         orbits = []
 
         # Insieme dei nodi non ancora visitati
-        unvisited_nodes = set(graph.nodes())
+        unvisited_nodes = set(self.nodes())
 
         # Finché ci sono nodi non visitati
         while unvisited_nodes:
@@ -120,7 +102,7 @@ class MultiDiGraph(nx.MultiDiGraph):
 
             # Verifica l'equivalenza con gli altri nodi
             for node in nodes_to_check:
-                if self.are_equivalent(start_node, node, graph):
+                if self.are_equivalent(start_node, node):
                     orbit.add(node)
                     unvisited_nodes.remove(node)
 
@@ -129,24 +111,24 @@ class MultiDiGraph(nx.MultiDiGraph):
         # Restituisce la lista delle orbite
         return orbits
 
-    def are_equivalent_edges(self, edge1, edge2, G):
+    def are_equivalent_edges(self, edge1, edge2):
 
         source1, target1 = edge1
         source2, target2 = edge2
 
         # Verifica se i nodi sorgente e destinazione hanno le stesse etichette
-        if set(G.nodes[source1]['label']) == set(G.nodes[source2]['label']) and \
-                set(G.nodes[target1]['label']) == set(G.nodes[target2]['label']):
+        if set(self.nodes[source1]['labels']) == set(self.nodes[source2]['labels']) and \
+                set(self.nodes[target1]['labels']) == set(self.nodes[target2]['labels']):
             # Verifica se gli archi hanno lo stesso tipo
-            if G.edges[edge1]['type'] == G.edges[edge2]['type']:
+            if self.edges[edge1]['type'] == self.edges[edge2]['type']:
                 return True
 
         return False
 
-    def compute_orbits_edges(self, graph):
+    def compute_orbits_edges(self):
 
         orbits = []
-        unvisited_edges = set(graph.edges())
+        unvisited_edges = set(self.edges())
 
         while unvisited_edges:
             start_edge = unvisited_edges.pop()
@@ -154,7 +136,7 @@ class MultiDiGraph(nx.MultiDiGraph):
             edges_to_check = unvisited_edges.copy()
 
             for edge in edges_to_check:
-                if self.are_equivalent_edges(start_edge, edge, graph):
+                if self.are_equivalent_edges(start_edge, edge):
                     orbit.add(edge)
                     unvisited_edges.remove(edge)
 
