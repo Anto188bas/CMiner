2d4db2aad0b19a29cfefa9240a4f7885a599a4f6
SimoneAvellino <s.avellino@gmail.com>
2024-04-09 13:23:40 +0200

    Update

--- Matching/Matching.py
+++ Matching/Matching.py
@@ -4,6 +4,7 @@ from BreakingConditions.BreakingConditions import BreakingConditionsNodes, Break
 from CompatibilityDomain.CompatibilityDomain import CompatibilityDomainWithDictionary
 from Ordering.Ordering import Ordering
 
+
 class MultiSubgraphMatching:
 
     def __init__(self, target, query):
@@ -26,75 +27,174 @@ class MultiSubgraphMatching:
 
     def match(self):
         self.ordering.compute()
+        forceBack = False
         i = 0
         q_i, q_j, q_key = query_edge = self.ordering.get(i)
         self._find_candidates(query_edge)
-        print("Cand(e_q)", query_edge)
         while i >= 0:
-
-            if self.cand_index[query_edge] >= len(self.cand[query_edge]):
-                print("CANDIDATES EXHAUSTED, BACKTRACKING")
-                # backtraking
-                print("  -Reset g[", query_edge, "] = None")
-                self.g[query_edge] = None
-                # reset f values
-                # reset f[q_i] if there are no edges in the query that have q_i as source/destination
-                # and are mapped to a target edge
-                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_i or q_e[1] == q_i):
-                    print("  -Reset f[", q_i, "]")
-                    self.f[q_i] = None
-                # reset f[q_j] if there are no edges in the query that have q_j as source/destination
-                # and are mapped to a target edge
-                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_j or q_e[1] == q_j):
-                    print("  -Reset f[", q_j, "]")
-                    self.f[q_j] = None
-
-
-
-                i -= 1
+            print("\n\nQuery edge: ", query_edge)
+            print("Candidates: ", self.cand[query_edge])
+            print("Index candidates: ", self.cand_index[query_edge])
+            if forceBack or self.cand_index[query_edge] >= len(self.cand[query_edge]):
+                print("BACKTRACKING")
+                forceBack = False
+                if self.g[query_edge] is None:
+                    i -= 1
                 if i < 0:
+                    # no more solutions
                     break
                 q_i, q_j, q_key = query_edge = self.ordering.get(i)
-                print("  -Cand(e_q)", query_edge)
+
+                # backtraking
+                # RESET THE MAPPING OF THE QUERY EDGE
+                print(self.g)
+                self.g[query_edge] = None
+                print("g[", query_edge, "] = None")
+                print(self.g)
+                # RESET THE MAPPING OF THE QUERY NODES
+                # reset f[q_i] if there are no mapped edges in the query that have q_i as source/destination
+                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_i or q_e[1] == q_i):
+                    print("Reset f[", q_i, "] = None")
+                    self.f[q_i] = None
+                # reset f[q_j] if there are no mapped edges in the query that have q_j as source/destination
+                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_j or q_e[1] == q_j):
+                    print("Reset f[", q_j, "] = None")
+                    self.f[q_j] = None
+                # take the previous edge in the ordering
+                # i -= 1
+                # if i < 0:
+                #     # no more solutions
+                #     break
+                # q_i, q_j, q_key = query_edge = self.ordering.get(i)
+                self.cand_index[query_edge] += 1  #################################
+
             else:
-                print("NEXT CANDIDATE")
+                # CHECK IF THE NEXT CANDIDATE TARGET EDGE IS COMPATIBLE WITH THE QUERY EDGE
+                # extract the target edge
                 t_i, t_j, key = target_edge = self.cand[query_edge][self.cand_index[query_edge]]
-                print("  -Cand(e_t):", target_edge)
+                print("Target edge: ", target_edge)
+                # check if the target edge has not been already mapped to another query edge
                 if all(self.g[q_e] != target_edge for q_e in self.query.get_all_edges() if self.g[q_e] is not None):
-                    print(" Target edge mappable")
+                    # if the mapping for the source is not already set, set it
                     if self.f[q_i] is None:
-                        print("  -f[", q_i, "] = ", t_i)
+                        print("Set f[", q_i, "] = ", t_i)
                         self.f[q_i] = t_i
+                    # if the mapping for the destination is not already set, set it
                     if self.f[q_j] is None:
-                        print("  -f[", q_j, "] = ", t_j)
+                        print("Set f[", q_j, "] = ", t_j)
                         self.f[q_j] = t_j
-                    print("  -g[", query_edge, "] = ", target_edge)
+                    # set the mapping for the query edge
+                    print("Set g[", query_edge, "] = ", target_edge)
                     self.g[query_edge] = target_edge
+                    # if the edge is the last in the ordering, the solution is found
                     if i == len(self.query.get_all_edges()) - 1:
-                        print("!!!!!!!!!!!!!!!!!!!!!!SOLUTION FOUND!!!!!!!!!!!!!!!!!!!!!!")
-                        # solution found
-                        self.occurrences.append(self.g.copy())
-                        self.cand_index[query_edge] += 1
+                        print("NEW SOLUTION FOUND")
+                        forceBack = True
+                        # SOLUTION FOUND
+                        # save the mapping
+                        self.occurrences.append({
+                            "f": self.f.copy(),
+                            "g": self.g.copy()
+                        })
+                        # shift the index to the next candidate
+                        # self.cand_index[query_edge] += 1
                     else:
                         print("NEXT QUERY EDGE")
+                        # shift the index to the next query edge in the ordering
                         i += 1
+                        # get the next query edge
                         q_i, q_j, q_key = query_edge = self.ordering.get(i)
-                        print("Cand(e_q)", query_edge)
+                        # find the candidates for the next query edge
                         self._find_candidates(query_edge)
+                        # reset the index for the candidates of the next query edge
                         self.cand_index[query_edge] = 0
                 else:
-                    print("Target edge not mappable")
+                    print("Already mapped")
+                    # shift the index to the next candidate
+                    self.cand_index[query_edge] += 1
+        print("SOLUTIONS FOUND: ", len(self.occurrences))
+        for occ in self.occurrences:
+            print("\n\nSolution:")
+            print("f: ", occ["f"])
+            print("g: ", occ["g"])
+
+    def match_no_print(self):
+        self.ordering.compute()
+        # used to force backtracking when a solution is found
+        force_backtracking = False
+        # index of the current query edge in the ordering
+        i = 0
+        # get the first query edge
+        q_i, q_j, q_key = query_edge = self.ordering.get(i)
+        # find the candidates for the first query edge
+        self._find_candidates(query_edge)
+        # loop until all the query edges have been mapped
+        while i >= 0:
+            # if the index of the candidates is greater than the number of candidates
+            # or the backtracking is forced
+            if force_backtracking or self.cand_index[query_edge] >= len(self.cand[query_edge]):
+                force_backtracking = False
+                if self.g[query_edge] is None:
+                    i -= 1
+                if i < 0:
+                    # no more solutions
+                    break
+                q_i, q_j, q_key = query_edge = self.ordering.get(i)
+
+                # backtraking
+                # RESET THE MAPPING OF THE QUERY EDGE
+                self.g[query_edge] = None
+                # RESET THE MAPPING OF THE QUERY NODES
+                # reset f[q_i] if there are no mapped edges in the query that have q_i as source/destination
+                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_i or q_e[1] == q_i):
+                    self.f[q_i] = None
+                # reset f[q_j] if there are no mapped edges in the query that have q_j as source/destination
+                if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_j or q_e[1] == q_j):
+                    self.f[q_j] = None
+                self.cand_index[query_edge] += 1
+
+            else:
+                # CHECK IF THE NEXT CANDIDATE TARGET EDGE IS COMPATIBLE WITH THE QUERY EDGE
+                # extract the target edge
+                t_i, t_j, key = target_edge = self.cand[query_edge][self.cand_index[query_edge]]
+                # check if the target edge has not been already mapped to another query edge
+                if all(self.g[q_e] != target_edge for q_e in self.query.get_all_edges() if self.g[q_e] is not None):
+                    # if the mapping for the source is not already set, set it
+                    if self.f[q_i] is None:
+                        self.f[q_i] = t_i
+                    # if the mapping for the destination is not already set, set it
+                    if self.f[q_j] is None:
+                        self.f[q_j] = t_j
+                    # set the mapping for the query edge
+                    self.g[query_edge] = target_edge
+                    # if the edge is the last in the ordering, the solution is found
+                    if i == len(self.query.get_all_edges()) - 1:
+                        force_backtracking = True
+                        # SOLUTION FOUND
+                        # save the mapping
+                        self.occurrences.append({
+                            "f": self.f.copy(),
+                            "g": self.g.copy()
+                        })
+                    else:
+                        # shift the index to the next query edge in the ordering
+                        i += 1
+                        # get the next query edge
+                        q_i, q_j, q_key = query_edge = self.ordering.get(i)
+                        # find the candidates for the next query edge
+                        self._find_candidates(query_edge)
+                        # reset the index for the candidates of the next query edge
+                        self.cand_index[query_edge] = 0
+                else:
+                    # shift the index to the next candidate
                     self.cand_index[query_edge] += 1
 
-
-
-
     def _find_candidates(self, query_edge):
-        print("Find candidates")
-        q_i, q_j, query_id = query_edge
+        q_i, q_j, query_key = query_edge
+        print("Find candidates for: ", query_edge)
         self.cand[query_edge] = []
         if self.f[q_i] is None and self.f[q_j] is None:
-            print("Source and destinaions not mapped")
+            print("1")
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
                 # each target edge is a tuple (source, target, edge_id)
                 for target_key in self.target.edges_keys((t_i, t_j)):
@@ -111,9 +211,10 @@ class MultiSubgraphMatching:
                     ):
                         self.cand[query_edge].append(target_edge)
         elif self.f[q_i] is not None and self.f[q_j] is not None:
-            print("Source and destinaions mapped")
+            print("2 AAAAAAAAAAAAAAAAAAAAAAAA")
             for target_key in self.target.edges_keys((self.f[q_i], self.f[q_j])):
                 target_edge = (self.f[q_i], self.f[q_j], target_key)
+                print("Target edge: ", target_edge)
                 if (
                         self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge) and
                         self.target.edge_contains_attributes(target_edge, self.query.get_edge_attributes(query_edge))
@@ -121,30 +222,35 @@ class MultiSubgraphMatching:
                     if self.br_cond_edge.check(query_edge, target_edge):
                         self.cand[query_edge].append(target_edge)
         elif self.f[q_i] is not None:
-            print("Only Source mapped")
+            print("3")
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
                 if t_i == self.f[q_i]:
                     for target_key in self.target.edges_keys((t_i, t_j)):
-                        target_edge = (self.f[q_i], t_j, target_key)
+                        target_edge = (t_i, t_j, target_key)
                         if (
-                            self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge) and
-                            self.target.node_contains_attributes(t_j, self.query.get_node_attributes(q_j)) and
-                            self.target.edge_contains_attributes(target_edge, self.query.get_edge_attributes(query_edge))
+                                self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge) and
+                                self.target.node_contains_attributes(t_j, self.query.get_node_attributes(q_j)) and
+                                self.target.edge_contains_attributes(target_edge,
+                                                                     self.query.get_edge_attributes(query_edge))
                         ):
                             if self.br_cond_node.check(q_j, t_j):
                                 self.cand[query_edge].append(target_edge)
         else:
-            print("Only Destination mapped")
+            print("4")
+            print(self.domain.get_domain((q_i, q_j)))
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
+                print((t_i, t_j))
+                print("aaa", t_j, self.f[q_j])
                 if t_j == self.f[q_j]:
+                    print("HERE")
                     for target_key in self.target.edges_keys((t_i, t_j)):
-                        target_edge = (t_i, self.f[q_j], target_key)
+                        target_edge = (t_i, t_j, target_key)
+
                         if (
                                 self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge) and
                                 self.target.node_contains_attributes(t_i, self.query.get_node_attributes(q_i)) and
-                                self.target.edge_contains_attributes(target_edge, self.query.get_edge_attributes(query_edge))
+                                self.target.edge_contains_attributes(target_edge,
+                                                                     self.query.get_edge_attributes(query_edge))
                         ):
                             if self.br_cond_node.check(q_i, t_i):
                                 self.cand[query_edge].append(target_edge)
-
-        print("find_candidates", query_edge, self.cand[query_edge])
