c1bb2ff0592ba2d40b1ea41465a8587af70cd8d2
LeleValore <emanuelevalore@gmail.com>
2024-07-14 14:05:19 +0200

    added control to mapping function in match method

--- src/CMiner/MultiGraphMatch.py
+++ src/CMiner/MultiGraphMatch.py
@@ -2,12 +2,8 @@ from src.CMiner.BitMatrix import QueryBitMatrixOptimized, TargetBitMatrixOptimiz
 from src.CMiner.BreakingConditions import BreakingConditionsNodes, BreakingConditionsEdges
 from src.CMiner.CompatibilityDomain import CompatibilityDomainWithDictionary
 from src.CMiner.Ordering import Ordering
-<<<<<<< HEAD
-import time
-=======
 import ray
 
->>>>>>> a5aebab156e565b1281d899a10ba7aa0f3777f21
 
 
 class Solution:
@@ -46,8 +42,6 @@ class Solution:
     def is_edge_mapped(self, edge):
         return edge in self.g.values()
 
-
-
     def __str__(self):
         str = "------------------------------------------\n"
         str += "Query node mapping:\n"
@@ -68,8 +62,6 @@ class MultiGraphMatch:
             self.tbm = TargetBitMatrixOptimized(self.target, BitMatrixStrategy2())
         else:
             self.tbm = target_bit_matrix
-        # all of this attributes are initialized in the match method
-        # so that the class can be reused
         self.query = None
         self.qbm = None
         self.node_mapping_function = None
@@ -90,8 +82,7 @@ class MultiGraphMatch:
                        compatibility_domain=None,
                        ordering=None,
                        breaking_conditions_nodes=None,
-                       breaking_conditions_edges=None
-                       ):
+                       breaking_conditions_edges=None):
         self.solutions = []
         self.query = query
         self.f = {node: None for node in query.nodes()}
@@ -119,7 +110,6 @@ class MultiGraphMatch:
         else:
             self.br_cond_edge = breaking_conditions_edges
 
-
     def match(self,
               query,
               query_bit_matrix=None,
@@ -145,76 +135,53 @@ class MultiGraphMatch:
                 if self.g[query_edge] is None:
                     i -= 1
                 if i < 0:
-                    # no more solutions
                     break
                 q_i, q_j, q_key = query_edge = self.ordering.get(i)
-
-                # backtracking
-                # RESET THE MAPPING OF THE QUERY EDGE
                 self.g[query_edge] = None
-                # RESET THE MAPPING OF THE QUERY NODES
-                # reset f[q_i] if there are no mapped edges in the query that have q_i as source/destination
                 if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_i or q_e[1] == q_i):
                     self.f[q_i] = None
-                # reset f[q_j] if there are no mapped edges in the query that have q_j as source/destination
                 if all(self.g[q_e] is None for q_e in self.query.get_all_edges() if q_e[0] == q_j or q_e[1] == q_j):
                     self.f[q_j] = None
                 self.cand_index[query_edge] += 1
             else:
-                # CHECK IF THE NEXT CANDIDATE TARGET EDGE IS COMPATIBLE WITH THE QUERY EDGE
-                # extract the target edge
                 t_i, t_j, key = target_edge = self.cand[query_edge][self.cand_index[query_edge]]
-                # check if the target edge has not been already mapped to another query edge
                 if all(self.g[q_e] != target_edge for q_e in self.query.get_all_edges() if self.g[q_e] is not None):
-                    # if the mapping for the source is not already set, set it
                     if self.f[q_i] is None:
+                        if t_i in self.f.values():
+                            self.cand_index[query_edge] += 1
+                            continue
                         self.f[q_i] = t_i
-                    # if the mapping for the destination is not already set, set it
                     if self.f[q_j] is None:
+                        if t_j in self.f.values():
+                            self.cand_index[query_edge] += 1
+                            continue
                         self.f[q_j] = t_j
-                    # set the mapping for the query edge
                     self.g[query_edge] = target_edge
-                    # if the edge is the last in the ordering, the solution is found
                     if i == len(self.query.get_all_edges()) - 1:
                         forceBack = True
-                        # SOLUTION FOUND
-                        # save the mapping
                         self.solutions.append(Solution(self.f.copy(), self.g.copy()))
                     else:
-                        # shift the index to the next query edge in the ordering
                         i += 1
-                        # get the next query edge
                         q_i, q_j, q_key = query_edge = self.ordering.get(i)
-                        # find the candidates for the next query edge
                         self._find_candidates(query_edge)
-                        # reset the index for the candidates of the next query edge
                         self.cand_index[query_edge] = 0
                 else:
-                    # shift the index to the next candidate
                     self.cand_index[query_edge] += 1
-
-
 
     def _find_candidates(self, query_edge):
         q_i, q_j, query_key = query_edge
         self.cand[query_edge] = []
         if self.f[q_i] is None and self.f[q_j] is None:
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
-                # each target edge is a tuple (source, target, edge_id)
                 for target_key in self.target.edges_keys((t_i, t_j)):
                     target_edge = (t_i, t_j, target_key)
                     if (
                             (
-                                    # if query edge label is not specified it means that any edge label is accepted
                                     not self.query.edge_has_label(query_edge) or
-                                    # check if the edge labels are the same
                                     self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge)
                             ) and
-                            # check if the target edge source node contains the same attributes as the query edge source node
                             self.target.node_contains_attributes(t_i, self.query.get_node_attributes(q_i)) and
-                            # check if the target edge destination node contains the same attributes as the query edge destination node
                             self.target.node_contains_attributes(t_j, self.query.get_node_attributes(q_j)) and
-                            # check if the target edge contains the same attributes as the query edge
                             self.target.edge_contains_attributes(target_edge,
                                                                  self.query.get_edge_attributes(query_edge))
                     ):
@@ -224,14 +191,11 @@ class MultiGraphMatch:
                 target_edge = (self.f[q_i], self.f[q_j], target_key)
                 if (
                         (
-                                # if query edge label is not specified it means that any edge label is accepted
                                 not self.query.edge_has_label(query_edge) or
-                                # check if the edge labels are the same
                                 self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge)
                         ) and
                         self.target.edge_contains_attributes(target_edge, self.query.get_edge_attributes(query_edge))
                 ):
-                    # if self.br_cond_edge.check(query_edge, target_edge):
                     self.cand[query_edge].append(target_edge)
         elif self.f[q_i] is not None:
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
@@ -240,30 +204,25 @@ class MultiGraphMatch:
                         target_edge = (t_i, t_j, target_key)
                         if (
                                 (
-                                        # if query edge label is not specified it means that any edge label is accepted
                                         not self.query.edge_has_label(query_edge) or
-                                        # check if the edge labels are the same
                                         self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge)
                                 ) and
                                 self.target.node_contains_attributes(t_j, self.query.get_node_attributes(q_j)) and
                                 self.target.edge_contains_attributes(target_edge,
                                                                      self.query.get_edge_attributes(query_edge))
                         ):
-                            # if self.br_cond_node.check(q_j, t_j):
                             self.cand[query_edge].append(target_edge)
         else:
             for t_i, t_j in self.domain.get_domain((q_i, q_j)):
                 if t_j == self.f[q_j]:
                     for target_key in self.target.edges_keys((t_i, t_j)):
                         target_edge = (t_i, t_j, target_key)
-
                         if (
                                 self.target.get_edge_label(target_edge) == self.query.get_edge_label(query_edge) and
                                 self.target.node_contains_attributes(t_i, self.query.get_node_attributes(q_i)) and
                                 self.target.edge_contains_attributes(target_edge,
                                                                      self.query.get_edge_attributes(query_edge))
                         ):
-                            # if self.br_cond_node.check(q_i, t_i):
                             self.cand[query_edge].append(target_edge)
 
     def get_solutions(self):
@@ -272,10 +231,9 @@ class MultiGraphMatch:
 
 @ray.remote
 def match_parallel_worker(target, query, worker_id):
-
     matcher = MultiGraphMatch(target, query)
     matcher.match(query)
-    solutions = matcher.solutions()
+    solutions = matcher.get_solutions()
     return solutions
 
 
